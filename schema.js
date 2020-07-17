183ed7deb091662727a94a401f233dc174feb72f
dlauck92 <drewlauck92@gmail.com>
2020-07-17 13:42:57 -0400

    pull data to display using bootstrap

--- schema.js
+++ schema.js
@@ -11,12 +11,12 @@ const axios = require("axios");
 
 // Launch Type
 const LaunchType = new GraphQLObjectType({
-  name: "Lanuch",
+  name: "Launch",
   fields: () => ({
     flight_number: { type: GraphQLInt },
     mission_name: { type: GraphQLString },
     launch_year: { type: GraphQLString },
-    launch_date: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
     launch_success: { type: GraphQLBoolean },
     rocket: { type: RocketType },
   }),
@@ -37,7 +37,7 @@ const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     // Get all launches
-    lanches: {
+    launches: {
       type: new GraphQLList(LaunchType),
       resolve(parent, args) {
         return axios
